5f5cf27bc54c4587cfbc736c77fee9cc0af7fdb1
wenghengcong <wenghengcong@gmail.com>
2015-09-20 10:28:23 +0800

    pod init

---
+++ JSBProjectBase/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JSBProjectBase
+//
+//  Created by wenghengcong on 15/9/20.
+//  Copyright © 2015年 JungleSong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JSBProjectBase/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JSBProjectBase
+//
+//  Created by wenghengcong on 15/9/20.
+//  Copyright © 2015年 JungleSong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
