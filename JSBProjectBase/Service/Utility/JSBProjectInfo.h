ed8f1341d0f6f0b4e5cf4fe39c8e765e133ad68e
wenghengcong <wenghengcong@gmail.com>
2016-01-05 04:13:46 +0800

    add uidevice helper file

---
+++ JSBProjectBase/Service/Utility/JSBProjectInfo.h
@@ -0,0 +1,13 @@
+//
+//  JSBProjectInfo.h
+//  JSBProjectBase
+//
+//  Created by wenghengcong on 16/1/4.
+//  Copyright © 2016年 JungleSong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JSBProjectInfo : NSObject
+
+@end
---
+++ JSBProjectBase/Service/Utility/JSBProjectInfo.m
@@ -0,0 +1,13 @@
+//
+//  JSBProjectInfo.m
+//  JSBProjectBase
+//
+//  Created by wenghengcong on 16/1/4.
+//  Copyright © 2016年 JungleSong. All rights reserved.
+//
+
+#import "JSBProjectInfo.h"
+
+@implementation JSBProjectInfo
+
+@end
