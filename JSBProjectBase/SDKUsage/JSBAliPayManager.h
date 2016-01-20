f327beaf44020f3f718b37c7b08d48a524f86a62
wenghengcong <wenghengcong@163.com>
2016-01-20 11:50:16 +0800

    add sharesdk adn pay lib,and alipay done

---
+++ JSBProjectBase/SDKUsage/JSBAliPayManager.h
@@ -0,0 +1,20 @@
+//
+//  JSBPayManager.h
+//  JSBProjectBase
+//
+//  Created by WengHengcong on 16/1/20.
+//  Copyright © 2016年 JungleSong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "AliOrder.h"
+
+/**
+ *  支付宝：
+ 文档中心：https://doc.open.alipay.com/doc2/detail.htm?spm=0.0.0.0.gjmRRl&treeId=59&articleId=103563&docType=1
+ 
+ */
+
+@interface JSBAliPayManager : NSObject
+
+@end
